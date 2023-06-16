e703a9022b74b0941083c3c9ea821174c9879056
JoyceEnidGL <zS19016410@estudiantes.uv.mx>
2023-06-16 17:53:19 -0600

    icono

--- MedicaMomento/app/src/main/java/com/example/medicamomento/Registros.kt
+++ MedicaMomento/app/src/main/java/com/example/medicamomento/Registros.kt
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class Registros : AppCompatActivity() {
+    private lateinit var medicamentos: List<DBhelper.Medicamento>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registros)
     }
+
 }
